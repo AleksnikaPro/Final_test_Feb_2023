a256eace7692eb74315096bc6b585088786d7aeb
Aleksandra <alexnika@bk.ru>
2023-02-24 10:03:04 +0300

    added tasks Coding

---
+++ Coding/Main.java
@@ -0,0 +1,11 @@
+import Classes.Zoo;
+import GUI.UI;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        UI.run();
+
+    }
+}
